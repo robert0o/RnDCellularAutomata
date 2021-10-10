8d71e29fb6722c4456fd99c5dea0620ac361124f
Robert <robert4game@gmail.com>
2021-10-10 10:35:35 +0200

    Section detection is now working

--- CellularHallway/Assets/_Cellular automata/MapGenerator.cs
+++ CellularHallway/Assets/_Cellular automata/MapGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
@@ -16,17 +17,11 @@ public class MapGenerator : MonoBehaviour
     [Range(0, 1)]
     public int rule0,rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8;
 
-    private void Start()
-    {
-        GenMap();
-    }
-
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            GenMap();
-        }
+        GenMap();
+
+        getSectionsLists();
     }
     private void GenMap()
     {
@@ -114,7 +109,7 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = yPos-1; y <= yPos +1; y++)
             {
-                if (x >= 0 && x < width && y >= 0 && y < hight)
+                if (IsInsideMap(x,y))
                 {
                     if (x != xPos || y != yPos)
                         wallCount += map[x, y];
@@ -138,10 +133,139 @@ public class MapGenerator : MonoBehaviour
         {
             for (int y = 0; y < hight; y++)
             {
-                Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+                switch (map[x, y])
+                {
+                    case 1:
+                        Gizmos.color = Color.black;
+                        break;
+                    case 0:
+                        Gizmos.color = Color.white;
+                        break;
+                    case 2:
+                        Gizmos.color = Color.red;
+                        break;
+                    default:
+                        Gizmos.color = Color.yellow;
+                        break;
+                }
                 Vector3 pos = new Vector3(x, y,0);
                 Gizmos.DrawCube(pos, Vector3.one);
             }
         }
+    }
+
+    public struct Tile
+    {
+        public int xPos, yPos;
+        public Tile(int x , int y)
+        {
+            xPos = x;
+            yPos = y;
+        }
+    }
+
+    void getSectionsLists()
+    {
+        RemoveSmallSections(1, 3, 0);
+        RemoveSmallSections(0, 3, 2);
+    }
+
+    void RemoveSmallSections(int targetState, int minimumSize, int changeTo)
+    {
+        List<List<Tile>> wallSections = GetSections(targetState);
+        foreach (List<Tile> section in wallSections)
+        {
+            if (section.Count <= minimumSize)
+            {
+                foreach (Tile tile in section)
+                {
+                    map[tile.xPos, tile.yPos] = changeTo;
+                }
+            }
+        }
+    }
+
+    List<List<Tile>> GetSections(int tileState)
+    {
+        List<List<Tile>> sections = new List<List<Tile>>();
+        int[,] VisitedTiles = new int[width, hight];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < hight; y++)
+            {
+                if(VisitedTiles[x,y] == 0 && map[x,y] == tileState)
+                {
+                    List<Tile> section = Setsection(x, y);
+                    sections.Add(section);
+                    foreach(Tile tile in section)
+                    {
+                        VisitedTiles[tile.xPos, tile.yPos] = 1;
+                    }
+                }
+            }
+        }
+        return sections;
+    }
+
+    List<Tile> Setsection(int startX, int startY)
+    {
+        List<Tile> section = new List<Tile>();
+        int[,] VisitedTiles = new int[width, hight];
+        Queue<Tile> SectionQueue = new Queue<Tile>();
+        SectionQueue.Enqueue(new Tile(startX, startY));
+        int TileState = map[startX, startY];
+        
+        VisitedTiles[startX, startY] = 1;
+        
+        
+        Tile tile;
+        while (SectionQueue.Count > 0)
+        {
+            tile = SectionQueue.Dequeue();
+            section.Add(tile);
+            /*for (int x = startX-1 ; x <= startX + 1; x++)
+            {
+                for (int y = startY - 1; y <= startY + 1; y++)
+                {
+                    if (IsInsideMap(x,y) && (x == tile.xPos || y == tile.yPos))
+                    {
+                        if(VisitedTiles[x,y] == 0 && map[x,y] == TileState)
+                        {
+                            VisitedTiles[x, y] = 1;
+                            SectionQueue.Enqueue(new Tile(x, y));
+                        }
+                    }
+                }
+            }*/
+            if (IsInsideMap(tile.xPos - 1, tile.yPos) && map[tile.xPos - 1, tile.yPos] == TileState && VisitedTiles[tile.xPos - 1, tile.yPos] == 0)
+            {
+                VisitedTiles[tile.xPos - 1, tile.yPos] = 1;
+                SectionQueue.Enqueue(new Tile(tile.xPos - 1, tile.yPos));
+            }
+            if (IsInsideMap(tile.xPos + 1, tile.yPos) && map[tile.xPos + 1, tile.yPos] == TileState && VisitedTiles[tile.xPos + 1, tile.yPos] == 0)
+            {
+                VisitedTiles[tile.xPos + 1, tile.yPos] = 1;
+                SectionQueue.Enqueue(new Tile(tile.xPos + 1, tile.yPos));
+            }
+            if (IsInsideMap(tile.xPos, tile.yPos - 1) && map[tile.xPos, tile.yPos - 1] == TileState && VisitedTiles[tile.xPos, tile.yPos - 1] == 0)
+            {
+                VisitedTiles[tile.xPos, tile.yPos - 1] = 1;
+                SectionQueue.Enqueue(new Tile(tile.xPos, tile.yPos - 1));
+            }
+            if (IsInsideMap(tile.xPos, tile.yPos + 1) && map[tile.xPos, tile.yPos + 1] == TileState && VisitedTiles[tile.xPos, tile.yPos + 1] == 0)
+            {
+                VisitedTiles[tile.xPos, tile.yPos + 1] = 1;
+                SectionQueue.Enqueue(new Tile(tile.xPos, tile.yPos + 1));
+            }
+        }
+
+        
+            return section;
+    }
+    
+
+    bool IsInsideMap(int x,int y)
+    {
+        return (x >= 0 && x < width && y >= 0 && y < hight);
     }
 }
